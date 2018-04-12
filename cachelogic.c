afc4cc7edc5f63e9179518e53205d377f98274e6
DLohmann <DLohmann@ucmerced.edu>
2018-04-11 19:19:28 -0700

    See I am working on it

--- cachelogic.c
+++ cachelogic.c
@@ -9,138 +9,151 @@ unsigned int uint_log2(word w);
 int randomint( int x );
 
 /*
-  This function allows the lfu information to be displayed
+	This function allows the lfu information to be displayed
 
-    assoc_index - the cache unit that contains the block to be modified
-    block_index - the index of the block to be modified
+	assoc_index - the cache unit that contains the block to be modified
+	block_index - the index of the block to be modified
 
-  returns a string representation of the lfu information
+	returns a string representation of the lfu information
  */
 char* lfu_to_string(int assoc_index, int block_index)
 {
-  /* Buffer to print lfu information -- increase size as needed. */
-  static char buffer[9];
-  sprintf(buffer, "%u", cache[assoc_index].block[block_index].accessCount);
+	/* Buffer to print lfu information -- increase size as needed. */
+	static char buffer[9];
+	sprintf(buffer, "%u", cache[assoc_index].block[block_index].accessCount);
 
-  return buffer;
+	return buffer;
 }
 
 /*
-  This function allows the lru information to be displayed
+	This function allows the lru information to be displayed
 
-    assoc_index - the cache unit that contains the block to be modified
-    block_index - the index of the block to be modified
+	assoc_index - the cache unit that contains the block to be modified
+	block_index - the index of the block to be modified
 
-  returns a string representation of the lru information
+	returns a string representation of the lru information
  */
 char* lru_to_string(int assoc_index, int block_index)
 {
-  /* Buffer to print lru information -- increase size as needed. */
-  static char buffer[9];
-  sprintf(buffer, "%u", cache[assoc_index].block[block_index].lru.value);
+	/* Buffer to print lru information -- increase size as needed. */
+	static char buffer[9];
+	sprintf(buffer, "%u", cache[assoc_index].block[block_index].lru.value);
 
-  return buffer;
+	return buffer;
 }
 
 /*
-  This function initializes the lfu information
+	This function initializes the lfu information
 
-    assoc_index - the cache unit that contains the block to be modified
-    block_number - the index of the block to be modified
+	assoc_index - the cache unit that contains the block to be modified
+	block_number - the index of the block to be modified
 
 */
 void init_lfu(int assoc_index, int block_index)
 {
-  cache[assoc_index].block[block_index].accessCount = 0;
+	cache[assoc_index].block[block_index].accessCount = 0;
 }
 
 /*
-  This function initializes the lru information
+	This function initializes the lru information
 
-    assoc_index - the cache unit that contains the block to be modified
-    block_number - the index of the block to be modified
+	assoc_index - the cache unit that contains the block to be modified
+	block_number - the index of the block to be modified
 
 */
 void init_lru(int assoc_index, int block_index)
 {
-  cache[assoc_index].block[block_index].lru.value = 0;
+	cache[assoc_index].block[block_index].lru.value = 0;
 }
 
 /*
-  This is the primary function you are filling out,
-  You are free to add helper functions if you need them
+	This is the primary function you are filling out,
+	You are free to add helper functions if you need them
 
-  @param addr 32-bit byte address
-  @param data a pointer to a SINGLE word (32-bits of data)
-  @param we   if we == READ, then data used to return
-              information back to CPU
+	@param addr 32-bit byte address
+	@param data a pointer to a SINGLE word (32-bits of data)
+	@param we	if we == READ, then data used to return
+				information back to CPU
 
-              if we == WRITE, then data used to
-              update Cache/DRAM
+				if we == WRITE, then data used to
+				update Cache/DRAM
 */
 void accessMemory(address addr, word* data, WriteEnable we)
 {
-  /* Declare variables here */
+	/* Declare variables here */
 
-  /* handle the case of no cache at all - leave this in */
-  if(assoc == 0) {
-    accessDRAM(addr, (byte*)data, WORD_SIZE, we);
-    return;
-  }
+	/* handle the case of no cache at all - leave this in */
+	if(assoc == 0) {
+		accessDRAM(addr, (byte*)data, WORD_SIZE, we);
+		return;
+	}
+	
+	 // How many bits are in the (physical) address.
+	 // If the cache is direct mapped, then 
+	int offsetLength = uint_log2(block_size);
+	int indexLength = uint_log2(set_count);
+	int tagLength = 32 - indexLength - offsetLength;
+	
+	int offsetMask =  (1<<(offsetLength)) - 1;
+	int indexMask  = ((1<<(indexLength )) - 1) << offsetLength;
+	int tagMask    = (((1<<(tagLength   )) - 1) << offsetLength) << indexLength;
+	
+	if (setCount == 1) {
+		//fully associative
+		
+	} else if (setCount == assoc) {
+		//Direct mapped
+		
+	}
+	
+	int tag, index, offset;
+	offset = addr & 
+	index = (addr >> uint_log2(block_size)) & uint_log2(set_count); //First "substring" address to block_size
+	tag = addr >> (uint_log2(block_size) + uint_log2(set_count));
+	
+	
+	cacheBlock block = cache[index].block[offset]; 
+	if (block.dirty) {
+		
+	} 
+	
+	/*
+	You need to read/write between memory (via the accessDRAM() function) and
+	the cache (via the cache[] global structure defined in tips.h)
 
-  /*
-  You need to read/write between memory (via the accessDRAM() function) and
-  the cache (via the cache[] global structure defined in tips.h)
+	Remember to read tips.h for all the global variables that tell you the
+	cache parameters
 
-  Remember to read tips.h for all the global variables that tell you the
-  cache parameters
+	The same code should handle random, LFU, and LRU policies. Test the policy
+	variable (see tips.h) to decide which policy to execute. The LRU policy
+	should be written such that no two blocks (when their valid bit is VALID)
+	will ever be a candidate for replacement. In the case of a tie in the
+	least number of accesses for LFU, you use the LRU information to determine
+	which block to replace.
 
-  The same code should handle random, LFU, and LRU policies. Test the policy
-  variable (see tips.h) to decide which policy to execute. The LRU policy
-  should be written such that no two blocks (when their valid bit is VALID)
-  will ever be a candidate for replacement. In the case of a tie in the
-  least number of accesses for LFU, you use the LRU information to determine
-  which block to replace.
+	Your cache should be able to support write-through mode (any writes to
+	the cache get immediately copied to main memory also) and write-back mode
+	(and writes to the cache only gets copied to main memory when the block
+	is kicked out of the cache.
 
-  Your cache should be able to support write-through mode (any writes to
-  the cache get immediately copied to main memory also) and write-back mode
-  (and writes to the cache only gets copied to main memory when the block
-  is kicked out of the cache.
+	Also, cache should do allocate-on-write. This means, a write operation
+	will bring in an entire block if the block is not already in the cache.
 
-  Also, cache should do allocate-on-write. This means, a write operation
-  will bring in an entire block if the block is not already in the cache.
+	To properly work with the GUI, the code needs to tell the GUI code
+	when to redraw and when to flash things. Descriptions of the animation
+	functions can be found in tips.h
+	*/
 
-  To properly work with the GUI, the code needs to tell the GUI code
-  when to redraw and when to flash things. Descriptions of the animation
-  functions can be found in tips.h
-  */
+	/* Start adding code here */
 
-  /* Start adding code here */
-
-  //Define and calculate fields
-  int index, offset, tag;
-  offset = addr & uint_log2(block_size);
-  index = (addr >> uint_log2(block_size)) & uint_log2(set_count); //First "substring" address to block_size
-  tag = addr >> (uint_log2(block_size) + uint_log2(set_count));
-
-  cacheBlock block = cache[index].block[offset];
-
-  //TODO: LRU stuff
-
-  //TODO: check tag
-
-  //TODO: Write back first before operating on this block
-  if (block.dirty)
-  {
-	  
-  }
+	//Define and calculate fields
+	
 
 
-
-  /* This call to accessDRAM occurs when you modify any of the
-     cache parameters. It is provided as a stop gap solution.
-     At some point, ONCE YOU HAVE MORE OF YOUR CACHELOGIC IN PLACE,
-     THIS LINE SHOULD BE REMOVED.
-  */
-  accessDRAM(addr, (byte*)data, WORD_SIZE, we);
+	/* This call to accessDRAM occurs when you modify any of the
+	 cache parameters. It is provided as a stop gap solution.
+	 At some point, ONCE YOU HAVE MORE OF YOUR CACHELOGIC IN PLACE,
+	 THIS LINE SHOULD BE REMOVED.
+	*/
+	accessDRAM(addr, (byte*)data, WORD_SIZE, we);
 }
