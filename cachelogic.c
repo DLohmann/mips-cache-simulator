a84e7336b8d52a95eab8a39a4d857ad8a6fc17d9
RoboMWM <robomwm@users.noreply.github.com>
2018-05-07 23:49:22 -0700

    also convert writeback to save in a block

--- cachelogic.c
+++ cachelogic.c
@@ -302,11 +302,9 @@ void accessMemory(address addr, word* data, WriteEnable we) {
 			
 			
 			// save every byte in block to replace. Save the entire block from the cache to memory (because it's about to be replaced)
+			TransferUnit transferUnit = uint_log2(block_size); //Determine how many bytes we need to copy from memory to fill the block.
 			int addrToSave = (((toReplace->tag)<<indexLength)<<offsetLength) | (index << offsetLength);
-			for (int i = 0; i < block_size; i++) {	// number of bytes in the block is block_size
-				accessDRAM(addrToSave, (byte*)&(toReplace->data[i]), BYTE_SIZE, WRITE);
-				addrToSave = addrToSave + i;
-			}
+			accessDRAM(addrToSave, (byte*)&(toReplace->data[i]), transferUnit, WRITE);
 			//at this point, the entire block should be saved to DRAM
 			//addrToSave = (tag << indexLength) << offsetLength;
 			//addrToSave = //addrToSave | (index << offsetLength);
